de9edd35a33e731107a0ca3d4df46f1cd48fa49c
Fox <cosmo8072@gmail.com>
2023-06-13 16:35:57 +0700

    umm test

--- src/api/hooks.c
+++ src/api/hooks.c
@@ -1,4 +1,13 @@
 #include "hooks.h"
+#include "vec.h"
+#include "object/descriptor.h"
 
 void api_on_descriptor_free(struct descriptor* desc) {
+  int i;
+  struct descriptor_field* field;
+  vec_foreach_ptr(&desc->fields, field, i)
+    free((void*) field->name);
+  
+  vec_deinit(&desc->fields);
+  free((void*) desc->name);
 }
