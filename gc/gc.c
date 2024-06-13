e1c92a5a62f28084b64e9dad9bf1d84acc63adee
Foxie Flakey <foxieflakey@gmail.com>
2024-06-13 16:21:50 +0700

    Few GC algorithmn changes

--- gc/gc.c
+++ gc/gc.c
@@ -1,6 +1,7 @@
+#include <stdatomic.h>
 #include <stdlib.h>
 
-#include <flup/data_structs/buffer/circular_buffer.h>
+#include <flup/data_structs/buffer.h>
 
 #include "memory/arena.h"
 #include "heap/generation.h"
@@ -9,8 +10,20 @@
 
 void gc_on_allocate(struct arena_block* block, struct generation* gen) {
   block->gcMetadata = (struct gc_block_metadata) {
-    .markBit = gen->gcState->flipColor ? 1 : 0
+    .markBit = !gen->gcState->mutatorMarkedBitValue
   };
+}
+
+void gc_on_write(struct arena_block* objectWhichIsGoingToBeOverwritten) {
+  struct gc_block_metadata* metadata = &objectWhichIsGoingToBeOverwritten->gcMetadata;
+  struct gc_per_generation_state* gcState = metadata->owningGeneration->gcState;
+  
+  bool prevMarkBit = atomic_exchange(&metadata->markBit, gcState->GCMarkedBitValue);
+  if (prevMarkBit == gcState->GCMarkedBitValue)
+    return;
+  
+  // Enqueue an pointer
+  flup_buffer_write_no_fail(gcState->mutatorMarkQueue, &objectWhichIsGoingToBeOverwritten, sizeof(void*));
 }
 
 struct gc_per_generation_state* gc_per_generation_state_new(struct generation* gen) {
@@ -22,7 +35,7 @@ struct gc_per_generation_state* gc_per_generation_state_new(struct generation* g
     .ownerGen = gen
   };
   
-  if (!(self->markQueue = flup_circular_buffer_new(GC_MARK_QUEUE_SIZE)))
+  if (!(self->mutatorMarkQueue = flup_buffer_new(GC_MARK_QUEUE_SIZE)))
     goto failure;
   return self;
 
@@ -35,7 +48,7 @@ void gc_per_generation_state_free(struct gc_per_generation_state* self) {
   if (!self)
     return;
   
-  flup_circular_buffer_free(self->markQueue);
+  flup_buffer_free(self->mutatorMarkQueue);
   free(self);
 }
 
--- gc/gc.h
+++ gc/gc.h
@@ -3,65 +3,98 @@
 
 /*
 
+First idea something roughly similiar to CMS GC
+
 The rough idea is making application
 to allocate new objects as live if it
 allocated during GC cycle and take
 snapshot of the root set so GC can scan
 it concurrently with the applications
 
+MultiThreadedQueue<Object> mutatorMarkQueue = new MultiThreadedQueue<>()
+
 fun startOfGCCycle()
+  // Take root snapshot (one of two STW pauses)
+  
   stop all application threads()
   take snapshot of gc root()
   // Flip applications's perspective so it create new
-  // object which colored black to GC's perspective
-  flip application's perspective of the colors()
+  // object which is marked to GC perspective
+  flip meaning of mark bit()
   resume all application threads()
   
-  mark the GC root recursively()
+  // Concurrent marking (works on root)
+  for obj in rootSnapshot do
+    obj.markRecursively()
+  end
   
+  // Remark phase (concurrent)
+  
+  // Process mark queue while application still queue-ing until
+  // the queue empty
+  for obj in mutatorMarkQueue do
+    obj.markRecursively()
+  end
+  
+  // Sweeping phase (concurrent)
   // After scanning the GC root white objects guarantee
-  // to be dead objects as newly allocated object is colored
-  // black to GC's perspective and there no way application
-  // can get reference to white objects
+  // to be dead objects as newly allocated object is marked
+  // to GC's perspective and there no way application
+  // can get reference to unmarked objects
   free all white objects()
   
-  // Flip GC's perspective of the colors so on next
-  // color currently black objects is now white
-  flip GC's perspective of the colors()
+  flip GC's meaning of the mark bit()
+end
+
+// `obj` is object which was about to be overwritten
+// also called when an root entry is overwritten
+fun writeBarrier(obj)
+  if obj is marked to GC then
+    return
+  end
+  
+  // So that the same object not requeued
+  set obj to be marked to GC
+  mutatorMarkQueue.enqueue(obj)
 end
 
 Extra notes:
 1. Object which were alive which became dead during marking process
   doesn't have a good way to handle it so leave it for next GC cycle
-2. Black and white object can be represented as one mark bit (where
-  marked would be black and unmarked would be white)
 */
 
 #include <stdatomic.h>
 
-#include <flup/data_structs/buffer/circular_buffer.h>
+#include <flup/data_structs/buffer.h>
 
-// 8 MiB mark queue size
+// 8 MiB mutator mark queue size
 #define GC_MARK_QUEUE_SIZE (8 * 1024 * 1024)
 
 struct generation;
 struct arena_block;
 
 struct gc_block_metadata {
+  struct generation* owningGeneration;
   // Meaning changes based on flipColor on per generation state
-  bool markBit;
+  atomic_bool markBit;
 };
 
 struct gc_per_generation_state {
   struct generation* ownerGen;
-  atomic_bool flipColor;
   
-  flup_circular_buffer* markQueue;
+  // What mark bit value correspond to marked (
+  // the meaning of the mark bit changes throughout
+  // lifetime)
+  bool mutatorMarkedBitValue;
+  bool GCMarkedBitValue;
+  
+  flup_buffer* mutatorMarkQueue;
 };
 
 struct gc_per_generation_state* gc_per_generation_state_new(struct generation* gen);
 void gc_per_generation_state_free(struct gc_per_generation_state* self);
 
 void gc_on_allocate(struct arena_block* block, struct generation* gen);
+void gc_on_write(struct arena_block* objectWhichIsGoingToBeOverwritten);
 
 #endif
