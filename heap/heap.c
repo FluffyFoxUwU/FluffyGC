418eeb0bce48e70035493a19ddbbd46b0d788df8
Foxie Flakey <foxieflakey@gmail.com>
2024-06-13 21:11:04 +0700

    Add necessary block and unblock

--- heap/heap.c
+++ heap/heap.c
@@ -1,8 +1,9 @@
-#include <flup/bug.h>
-#include <flup/container_of.h>
 #include <stdlib.h>
 #include <stddef.h>
 
+#include <flup/bug.h>
+#include <flup/concurrency/mutex.h>
+#include <flup/container_of.h>
 #include <flup/core/logger.h>
 #include <flup/data_structs/list_head.h>
 
@@ -47,17 +48,21 @@ struct root_ref* heap_root_dup(struct heap* self, struct root_ref* ref) {
   if (!newRef)
     return NULL;
   
+  heap_block_gc(self);
   flup_list_add_tail(&self->root, &newRef->node);
   newRef->obj = ref->obj;
   self->rootEntryCount++;
+  heap_unblock_gc(self);
   return newRef;
 }
 
 void heap_root_unref(struct heap* self, struct root_ref* ref) {
+  heap_block_gc(self);
   self->rootEntryCount--;
   flup_list_del(&ref->node);
   
   gc_on_reference_lost(ref->obj);
+  heap_unblock_gc(self);
   free(ref);
 }
 
@@ -66,6 +71,7 @@ struct root_ref* heap_alloc(struct heap* self, size_t size) {
   if (!ref)
     return NULL;
   
+  heap_block_gc(self);
   struct arena_block* newObj = generation_alloc(self->gen, size);
   for (int i = 0; i < HEAP_ALLOC_RETRY_COUNT && newObj == NULL; i++) {
     pr_info("Allocation failed trying calling GC #%d", i + 1);
@@ -82,6 +88,7 @@ struct root_ref* heap_alloc(struct heap* self, size_t size) {
   flup_list_add_tail(&self->root, &ref->node);
   ref->obj = newObj;
   self->rootEntryCount++;
+  heap_unblock_gc(self);
   return ref;
 }
 
