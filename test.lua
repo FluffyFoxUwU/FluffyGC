433f49ebfe956636204a7d0283d8ed6047bebb7f
Foxie Flakey <foxieflakey@gmail.com>
2024-07-13 16:43:43 +0700

    Update the test UwU

--- test.lua
+++ test.lua
@@ -1,30 +1,37 @@
-collectgarbage("incremental")
+local WINDOW_SIZE = 200000
+local MESSAGE_COUNT = 1000000
+local MESSAGE_SIZE = 1024
 
-function test()
-  local t = {}
+local function createMessage(n)
+  return (string.char(n % 0x100)..
+          string.char(math.floor(n / 8) % 0x100)..
+          string.char(math.floor(n / 16) % 0x100)..
+          string.char(math.floor(n / 24) % 0x100)):rep(MESSAGE_SIZE / 4)
+end
 
-  for i=1,1000 do
-    t[i] = {}
-    for j=1,100 do
-      local var = {}
-      var[1] = {812}
-      var[2] = {var}
-        
-      local var2 = {}
-      var2[1] = {1, 2, 3}
-      var2[2] = {452}
-      t[i][j] = var
-    end
+local worstPushTime = 0
+local function pushMessage(store, id)
+  local start, ending
+  start = os.clock()
+  store[id % WINDOW_SIZE] = createMessage(id)
+  ending = os.clock()
+  local time = ending - start
+  if time > worstPushTime then
+    worstPushTime = time
   end
 end
 
-local startTime = os.clock()
-for i=1,20 do
-  test()
+local store = {}
+for i=0, WINDOW_SIZE-1 do
+  store[i] = 0
 end
-collectgarbage("collect")
-local endTime = os.clock()
 
-print("Time taken was", endTime - startTime)
-
+local start, ending
+start = os.clock()
+for i = 0, MESSAGE_COUNT-1 do
+  pushMessage(store, i)
+end
+ending = os.clock()
+print("Test took ", ending - start, " seconds")
+print("Worst time was ", worstPushTime)
 
