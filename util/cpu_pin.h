0d0c9d2dabb7ed8a2e65b871122051feaba67e5a
Foxie Flakey <foxieflakey@gmail.com>
2024-08-26 17:12:59 +0700

    Remove CPU pinning, its currently garbage from long forgotten test

--- util/cpu_pin.h
+++
@@ -1,9 +0,0 @@
-#ifndef UWU_CE719EB9_3F08_40E5_9881_38F580A8C20C_UWU
-#define UWU_CE719EB9_3F08_40E5_9881_38F580A8C20C_UWU
-
-#include <pthread.h>
-
-// Return true on success, false on failure and negative on unsupported
-int util_cpu_pin_try_pin(pthread_t thrd, unsigned int cpuID);
-
-#endif
