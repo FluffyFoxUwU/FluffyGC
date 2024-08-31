2b93bfcb699b56604307475384112eb8e6a728aa
Foxie Flakey <foxieflakey@gmail.com>
2024-08-31 16:07:54 +0700

    Remove keep'ed headers

--- api/common.h
+++ api/common.h
@@ -1,16 +1,12 @@
 #ifndef UWU_FA033D7E_E7E3_4C9B_9416_7F5E53507E54_UWU
 #define UWU_FA033D7E_E7E3_4C9B_9416_7F5E53507E54_UWU
 
-// These used by the macros
-#include <FluffyGC/FluffyGC.h> // IWYU pragma: keep
-#include "heap/heap.h" // IWYU pragma: keep
-
 #define API_INTERN(x) _Generic((x), \
-  fluffygc_state*: (struct heap*) (x) \
+  struct fluffygc_state*: (struct heap*) (x) \
 )
 
 #define API_EXTERN(x) _Generic((x), \
-  struct heap*: (fluffygc_state*) (x) \
+  struct heap*: (struct fluffygc_state*) (x) \
 )
 
 #endif
